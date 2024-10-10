112ad4fb98d6c316e3ee84c32bcdfcdfc0123ac7
ShT9m <git@stackblitz.com>
2024-10-10 14:25:25 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# udemy-JS-and-React
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/ShT9m/udemy-JS-and-React)
---
+++ main.js
@@ -0,0 +1,12 @@
+/**
+ * const, let等の変数宣言
+ */
+var val1 = 'var変数';
+console.log(var1);
+
+val1 = 'var変数を上書き';
+console.log(val1);
+
+// var変数は再宣言可能
+val1 = 'var変数を再宣言';
+console.log(val1);
